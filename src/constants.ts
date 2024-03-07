44b72a32c500469b5344421fd0079e058441b238
fitdevproject <fitdevproject@gmail.com>
2024-03-07 13:34:39 -0500

    init commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Tech With Tmont";
+export const SITE_DESCRIPTION = "Trevor Montgomery's portfolio and tech blog";
+export const HOMEPAGE_BLOG_LIMIT = 3;
+export const BLOGS_PER_PAGE = 6;
